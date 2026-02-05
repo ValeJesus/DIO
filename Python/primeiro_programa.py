3ba8fc8d0ea4cad9ce0d04c3e3988e6cab36d721
João vitor <joaodraw5@gmail.com>
2026-02-04 21:53:29 -0300

    Introdução ao python, variaveis, comandos no terminal etc

---
+++ Python/primeiro_programa.py
@@ -0,0 +1,4 @@
+nome = input("Informe o seu nome:")
+idade = input("Informe a sua idade:")
+
+print(f"Seu nome é {nome}, e Você tem {idade} anos")
---
+++ Python/variaveis_constantes.py
@@ -0,0 +1,49 @@
+nome, idade = "Guilherme", 30
+
+print(nome, idade)
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+BRAZILIAN_STATES = ['SP', 'RJ', 'MG', 'SC']
+
+print(BRAZILIAN_STATES)
+
+
+#conversão de float para inteiro
+#1:
+preco = 10.30
+print(preco)
+
+preco = int(preco)
+print(preco)
+#2:
+preco = 10
+print(preco)
+
+print(preco / 2)
+#sem as duas barras o valor fica float, com as duas barras o valor fica inteiro
+print(preco // 2)
+
+#----------------------------
+#Numericos para string
+preco = 10.50
+idade = 28
+
+print(str(preco))
+print(str(idade))
+
+texto = f"idade {idade} preço {preco}"
+print(texto)
+
+#----------------------------
+#string pra numerico
+
+preco = "10.50"
+idade = "28"    
+
+print(float(preco))
+print(int(idade))
+
+print(type(preco))
